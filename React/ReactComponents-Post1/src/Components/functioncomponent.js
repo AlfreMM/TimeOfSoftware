736147a950c10791cb2009e0cf2ba0cdc8510782
Alfre <amoreno@tunstall.es>
2020-05-08 08:42:13 +0200

    Add files

---
+++ React/ReactComponents-Post1/src/Components/functioncomponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FunctionComponent = function(props) {
+  return (
+    <div>
+      <p>This is function based component</p>
+    </div>
+  );
+};
+
+export default FunctionComponent;
